426c970140f568fc2c6d0b42984121d2ba7cb631
mizt <info@mizt.org>
2024-02-25 13:04:13 +0900

    Initial commit

---
+++ main.mm
@@ -0,0 +1,26 @@
+#import <Foundation/Foundation.h>
+#import "zstd.h"
+
+int main(int argc, char *argv[]) {
+	@autoreleasepool {
+		NSString *src = @"76.glb";
+		NSData *glb = [[NSData alloc] initWithContentsOfFile:[NSString stringWithFormat:@"./",src]];
+		if(glb) {
+			unsigned int length = glb.length;
+			unsigned char *buf = new unsigned char[length];
+			NSMutableData *dst = [[NSMutableData alloc] init];
+			unsigned int ret = (unsigned int)ZSTD_compress(buf,length,glb.bytes,length,1);
+			if(ret>=1) {
+				[dst appendBytes:new unsigned int[1]{length} length:4];
+				[dst appendBytes:new const char[4]{'z','s','t','d'} length:4];
+				[dst appendBytes:buf length:ret];
+				[dst writeToFile:[NSString stringWithFormat:@"./docs/%@.zstd",src] atomically:YES];
+			}
+			else {
+				NSLog(@"%d",ret);
+			}
+			if(buf) delete[] buf;
+			glb = nil;
+		}
+	}
+}
